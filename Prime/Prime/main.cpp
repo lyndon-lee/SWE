4d1f6cd37f713e00c770632480318d34b4388fd1
dongge <lyndon.leeseu@gmail.com>
2015-10-27 19:22:25 +0800

    MemCopy don't need to 2 3 5 7 11 13

--- Prime/Prime/main.cpp
+++ Prime/Prime/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
-int n = 600000000;
-bool* isPrimes = new bool[n + 1];
+int n = 900900000;
+//2*3*5*7*11
+int seg = 30030;
+bool* isPrimes = new bool[n+1];
+bool* segb = new bool[seg+1];
+void filterSeg(){
+	memset(segb, true, seg+1);
+	segb[2] = true;
+	for (int j = 2; j <= 13; ++j)
+	{
+		if (segb[j] == true)
+		{
+			for (int m = 2; j*m <= seg; ++m)
+			{
+				segb[j*m] = false;
+			}
+		}
+	}
+}
 void FilterPrime()
 {
-	for (int i = 2; i <= n; ++i)
-	{
-		isPrimes[i] = true;
+	memset(isPrimes, true, n+1);
+	memcpy(isPrimes, segb, seg+1);
+	for (int i = 1; i < 30000;i++){
+		memcpy(isPrimes+i*seg+1, segb+1, seg);
 	}
-	isPrimes[2] = true;
-	for (int j = 2; j <= n; ++j)
+
+	for (int j = 17; j <= n; ++j)
 	{
 		if (isPrimes[j] == true)
 		{
@@ -20,42 +38,82 @@ void FilterPrime()
 		}
 	}
 }
+
 int main()
 {
 	int num;
-	FilterPrime();	
-	while(cin >> num){
-	int count = 0;
-	int i = 2;
-	for (; i < n;i++){
-		if (isPrimes[i]){
-			count++;
+	filterSeg();
+	FilterPrime();
+	while (cin >> num){
+		int count = 0;
+		int i = 2;
+		for (; i < n; i++){
+			if (isPrimes[i]){
+				count++;
+			}
+			if (count == num)
+				break;
 		}
-		if (count == num)
-			break;
+		cout << i << endl;
 	}
-	cout << i << endl;
-	}
-	return 0;
 }
+//#include <stdio.h>
+//#include <string.h>
+//#include <limits.h>  
 //#include<iostream>
+//
+//#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+//#define BITSLOT(b) ((b) / CHAR_BIT)
+//#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+//#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+//#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+//#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+//#define MAX 90090000*8
+//#define seg 30030*8
 //using namespace std;
-//const long N = 20000;
-//long prime[N] = { 0 }, num_prime = 0;
-//bool isNotPrime[N] = { true, true };
-//int main()
-//{
-//	for (long i = 2; i < N; i++)
-//	{
-//		if (!isNotPrime[i])
-//			prime[num_prime++] = i;
-//		//¹Ø¼ü´¦1        
-//		for (long j = 0; j < num_prime && i * prime[j] < N; j++)
-//		{
-//			isNotPrime[i * prime[j]] = 1;
-//			if (!(i % prime[j]))  //¹Ø¼ü´¦2                  
-//				break;
+////2*3*5*7*11
+//
+//char bitarray[BITNSLOTS(MAX)];
+//char segb[BITNSLOTS(seg)];
+//void filterSeg(){
+//	memset(segb, 0, BITNSLOTS(seg));
+//	int i, j;
+//	for (i = 2; i < 13; i++) {
+//		if (!BITTEST(segb, i)) {
+//			for (j = i + i; j < seg; j += i)
+//				BITSET(segb, j);
 //		}
 //	}
+//}
+//int main()
+//{
+//	filterSeg();
+//	memset(bitarray, 0, BITNSLOTS(MAX));
+//	for (int i = 0; i < 3000; i++){
+//		memcpy(bitarray + i*BITNSLOTS(seg), segb, BITNSLOTS(seg));
+//	}
+//	int i, j;
+//
+//	for (i = 17; i < MAX; i++) {
+//		if (!BITTEST(bitarray, i)) {
+//			for (j = i + i; j < MAX; j += i)
+//				BITSET(bitarray, j);
+//		}
+//	}
+//	int num = 0;
+//	while (cin >> num){
+//		int count = 0;
+//		int i = 2;
+//		for (; i <MAX; i++){
+//			if (!BITTEST(bitarray, i)){
+//				count++;
+//			}
+//			if (count == num)
+//				break;
+//		}
+//		cout << i << endl;
+//	}
+//	cin.get();
 //	return 0;
 //}
+
