67c2389b58d6ac4e975cdda774d1c272876d22b9
dongge <lyndon.leeseu@gmail.com>
2015-11-03 13:55:09 +0800

    primeQuick

--- Prime/Prime/main.cpp
+++ Prime/Prime/main.cpp
@@ -1,119 +1,195 @@
 #include <iostream>
 using namespace std;
-int n = 900900000;
-//2*3*5*7*11
-int seg = 30030;
-bool* isPrimes = new bool[n+1];
-bool* segb = new bool[seg+1];
-void filterSeg(){
-	memset(segb, true, seg+1);
-	segb[2] = true;
-	for (int j = 2; j <= 13; ++j)
-	{
-		if (segb[j] == true)
-		{
-			for (int m = 2; j*m <= seg; ++m)
-			{
-				segb[j*m] = false;
-			}
-		}
-	}
-}
-void FilterPrime()
-{
-	memset(isPrimes, true, n+1);
-	memcpy(isPrimes, segb, seg+1);
-	for (int i = 1; i < 30000;i++){
-		memcpy(isPrimes+i*seg+1, segb+1, seg);
-	}
+int n = 600000000;
+bool* isPrimes = new bool[n + 1];
 
-	for (int j = 17; j <= n; ++j)
+int upBounds[70] =
+{
+	10,
+	30,
+	50,
+	70,
+	90,
+	100,
+	200,
+	300,
+	400,
+	500,
+	600,
+	700,
+	800,
+	900,
+	1000,
+	2000,
+	3000,
+	4000,
+	5000,
+	6000,
+	7000,
+	8000,
+	9000,
+	10000,
+	20000,
+	30000,
+	40000,
+	50000,
+	60000,
+	70000,
+	80000,
+	90000,
+	100000,
+	200000,
+	300000,
+	400000,
+	500000,
+	600000,
+	700000,
+	800000,
+	900000,
+	1000000,
+	2000000,
+	3000000,
+	4000000,
+	5000000,
+	6000000,
+	7000000,
+	8000000,
+	9000000,
+	10000000,
+	20000000,
+	30000000,
+	40000000,
+	50000000,
+	60000000,
+	70000000,
+	80000000,
+	90000000,
+	100000000,
+	200000000,
+	300000000,
+	400000000,
+	500000000,
+	600000000,
+	700000000,
+	800000000,
+	900000000,
+	1000000000,
+	2000000000
+};
+int primeNumber[70] = {
+	4,
+	10,
+	15,
+	19,
+	24,
+	25,
+	46,
+	62,
+	78,
+	95,
+	109,
+	125,
+	139,
+	154,
+	168,
+	303,
+	430,
+	550,
+	669,
+	783,
+	900,
+	1007,
+	1117,
+	1229,
+	2262,
+	3245,
+	4203,
+	5133,
+	6057,
+	6935,
+	7837,
+	8713,
+	9592,
+	17984,
+	25997,
+	33860,
+	41538,
+	49098,
+	56543,
+	63951,
+	71274,
+	78498,
+	148933,
+	216816,
+	283146,
+	348513,
+	412849,
+	476648,
+	539777,
+	602489,
+	664579,
+	1270607,
+	1857859,
+	2433654,
+	3001134,
+	3562115,
+	4118064,
+	4669382,
+	5216954,
+	5761455,
+	11078937,
+	16252325,
+	21336326,
+	26355867,
+	31324703,
+	36252931,
+	41146179,
+	46009215,
+	50847534,
+	98222287
+};
+
+
+void FilterPrime(int upbound)
+{
+	for (int i = 2; i <= upbound; ++i)
+	{
+		isPrimes[i] = true;
+	}
+	isPrimes[2] = true;
+	for (int j = 2; j <= upbound; ++j)
 	{
 		if (isPrimes[j] == true)
 		{
-			for (int m = 2; j*m <= n; ++m)
+			for (int m = 2; j*m <= upbound; ++m)
 			{
 				isPrimes[j*m] = false;
 			}
 		}
 	}
 }
-
 int main()
 {
 	int num;
-	filterSeg();
-	FilterPrime();
-	while (cin >> num){
-		int count = 0;
-		int i = 2;
-		for (; i < n; i++){
-			if (isPrimes[i]){
-				count++;
-			}
-			if (count == num)
-				break;
+	cin >> num;
+	int offset = 0;
+//	cout << primeNumber[69] << endl;
+	for (int i = 0; i < 70; i++){
+		if (num<primeNumber[i]){
+			offset = i;
+			break;
 		}
-		cout << i << endl;
 	}
+	FilterPrime(upBounds[offset]);
+	int count = 0;
+	int i = 2;
+	for (; i < upBounds[offset]; i++){
+		if (isPrimes[i]){
+			count++;
+		}
+		if (count == num)
+			break;
+	}
+	cout << i << endl;
+	return 0;
 }
-//#include <stdio.h>
-//#include <string.h>
-//#include <limits.h>  
-//#include<iostream>
-//
-//#define BITMASK(b) (1 << ((b) % CHAR_BIT))
-//#define BITSLOT(b) ((b) / CHAR_BIT)
-//#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
-//#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
-//#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
-//#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
-//#define MAX 90090000*8
-//#define seg 30030*8
-//using namespace std;
-////2*3*5*7*11
-//
-//char bitarray[BITNSLOTS(MAX)];
-//char segb[BITNSLOTS(seg)];
-//void filterSeg(){
-//	memset(segb, 0, BITNSLOTS(seg));
-//	int i, j;
-//	for (i = 2; i < 13; i++) {
-//		if (!BITTEST(segb, i)) {
-//			for (j = i + i; j < seg; j += i)
-//				BITSET(segb, j);
-//		}
-//	}
-//}
-//int main()
-//{
-//	filterSeg();
-//	memset(bitarray, 0, BITNSLOTS(MAX));
-//	for (int i = 0; i < 3000; i++){
-//		memcpy(bitarray + i*BITNSLOTS(seg), segb, BITNSLOTS(seg));
-//	}
-//	int i, j;
-//
-//	for (i = 17; i < MAX; i++) {
-//		if (!BITTEST(bitarray, i)) {
-//			for (j = i + i; j < MAX; j += i)
-//				BITSET(bitarray, j);
-//		}
-//	}
-//	int num = 0;
-//	while (cin >> num){
-//		int count = 0;
-//		int i = 2;
-//		for (; i <MAX; i++){
-//			if (!BITTEST(bitarray, i)){
-//				count++;
-//			}
-//			if (count == num)
-//				break;
-//		}
-//		cout << i << endl;
-//	}
-//	cin.get();
-//	return 0;
-//}
-
